b131393b3103eebe70ae09ebce2f936aaf701b53
Moi-nune <105731475+Moi-nune@users.noreply.github.com>
2024-06-27 01:16:15 +0530

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -10,8 +10,8 @@ starchconfirm = os.environ['starchconfirm']
 clogconfirm = os.environ['clogconfirm']
 spawnlogconfirm = os.environ['spawnlogconfirm']
 
-rping = os.environ['rping']
-regping = os.environ['regping']
-starch = os.environ['starch']
-clog = os.environ['clog']
-spawnlog = os.environ['spawnlog']
+rping = int(os.environ['rping'])
+regping = int(os.environ['regping'])
+starch = int(os.environ['starch'])
+clog = int(os.environ['clog'])
+spawnlog = int(os.environ['spawnlog'])
